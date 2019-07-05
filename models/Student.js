e118cae425e42fb21365075b55958ca219ffa1d2
mangelosalvio <msalvio@Mariss-iMac.local>
2019-07-05 20:35:59 +0800

    add student to add absent

--- models/Student.js
+++ models/Student.js
@@ -2,14 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const StudentSchema = new Schema({
-  id_number: {
-    type: String,
-    required: true
-  },
-  name: {
-    type: String,
-    required: true
-  }
+  id_number: String,
+  name: String,
+  absences: [Date]
 });
 
 module.exports = mongoose.model("students", StudentSchema);
--- routes/api/students.js
+++ routes/api/students.js
@@ -1,12 +1,44 @@
 const express = require("express");
 const router = express.Router();
 const Student = require("./../../models/Student");
+const moment = require("moment-timezone");
+
+router.get("/:id_number", (req, res) => {
+  Student.findOne({
+    id_number: req.params.id_number
+  }).then(student => res.json(student));
+});
 
 router.get("/", (req, res) => {
   Student.find().then(students => {
     setTimeout(() => {
       return res.json(students);
     }, 3000);
+  });
+});
+
+router.put("/:id_number/absent", (req, res) => {
+  Student.findOne({
+    id_number: req.params.id_number
+  }).then(student => {
+    if (student) {
+      const absences = [
+        moment.tz(moment(), process.env.TIMEZONE).toDate(),
+        ...student.absences
+      ];
+      student.set({
+        absences
+      });
+
+      student
+        .save()
+        .then(newStudent => {
+          return res.json(newStudent);
+        })
+        .catch(err => console.log(err));
+    } else {
+      return res.json({ msg: "ID # not found" });
+    }
   });
 });
 
